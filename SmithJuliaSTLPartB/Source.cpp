e16a98e04afca541edf5c5a8ee2c16488544f0a9
Julia <julia.julia@julia>
2020-12-09 14:47:56 -0500

    Add project files.

---
+++ SmithJuliaSTLPartB/Source.cpp
@@ -0,0 +1,21 @@
+//Julia Smith
+//CIS 1202
+//December 2020
+
+#include <iostream>
+using namespace std;
+
+//Function Template!
+template <class Half>
+Half half (Half num)
+{
+	return num / 2;
+}
+
+
+
+int main()
+{
+	//Test data!
+
+}
